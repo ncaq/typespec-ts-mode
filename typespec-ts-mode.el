e4774b3b8e5b8a49eadf276507c20390f9d07be2
ncaq <ncaq@ncaq.net>
2024-11-25 20:00:35 +0900

    feat: insert first template

---
+++ typespec-ts-mode.el
@@ -0,0 +1,36 @@
+;;; typespec-ts-mode.el --- Major mode for TypeSpec using tree-sitter  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 ncaq.
+
+;; Author: ncaq <ncaq@ncaq.net>
+;; Maintainer: ncaq <ncaq@ncaq.net>
+;; URL: https://github.com/ncaq/typespec-ts-mode
+
+;; Package: typespec-ts-mode
+;; Keywords: typespec languages tree-sitter
+
+;; Package-Version: 0.0.0
+;; Package-Requires: ((emacs "29.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Major mode for [TypeSpec](https://typespec.io/) using [tree-sitter](https://tree-sitter.github.io/tree-sitter/).
+
+;;; Code:
+
+;;; typespec-ts-mode.el ends here
